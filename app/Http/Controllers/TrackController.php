0680063752ef1d70652ad9d3a62696c0d751b843
adil <adil.200191@gmail.com>
2026-05-06 19:37:37 +0500

    fix tracking cities

--- app/Http/Controllers/TrackController.php
+++ app/Http/Controllers/TrackController.php
@@ -11,7 +11,7 @@ class TrackController extends Controller
     public function store(Request $request)
     {
         $ip = $request->ip();
-        $geo = Http::get("http://ip-api.com/json/{$ip}")->json();
+        $geo = Http::get("https://ip-api.co/{$ip}/json")->json();
         $hour = now()->startOfHour();
 
         Visit::updateOrCreate(
